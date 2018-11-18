a2d4b29caa928b44b48d8dad7eb434ae432069b8
M Saif Ullah <chsaif369@gmail.com>
2018-11-18 18:49:53 +0500

    Note list layout(recycler view) & note Editor activity added.

---
+++ app/src/main/java/com/saifi369/olschoolnotes/EditorActivity.java
@@ -0,0 +1,21 @@
+package com.saifi369.olschoolnotes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/saifi369/olschoolnotes/MainActivity.java
+++ app/src/main/java/com/saifi369/olschoolnotes/MainActivity.java
@@ -1,8 +1,15 @@
 package com.saifi369.olschoolnotes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -10,21 +17,31 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.textView)
-    TextView textView;
+    @BindView(R.id.notes_recyclerview)
+    RecyclerView mRecyclerView;
 
-    @OnClick(R.id.button)
-    void btnClicked(){
-        textView.setText("Hello from Java file");
+    @OnClick(R.id.fab_add_note)
+    void onFabClicked(){
+        Intent intent=new Intent(MainActivity.this,EditorActivity.class);
+        startActivity(intent);
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ButterKnife.bind(this);
+        initRecyclerView();
 
     }
+
+    private void initRecyclerView() {
+        mRecyclerView.hasFixedSize();
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+    }
+
 }
