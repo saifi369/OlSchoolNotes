8ce54c76d315615c35d57e1f598fa479a908d0cd
M Saif Ullah <chsaif369@gmail.com>
2018-11-18 17:20:49 +0500

    Launcher Icon, Butter Knife & Arch Components Dependencies

--- app/src/main/java/com/saifi369/olschoolnotes/MainActivity.java
+++ app/src/main/java/com/saifi369/olschoolnotes/MainActivity.java
@@ -1,13 +1,30 @@
 package com.saifi369.olschoolnotes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @OnClick(R.id.button)
+    void btnClicked(){
+        textView.setText("Hello from Java file");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
 }
